b3afd176ea42c2cdf4077ab3c577683d15de4cd6
Sravani1367 <sravanipentakota@gmail.com>
2025-07-19 18:25:20 +0530

    Initial commit

---
+++ hi.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <h1>My first php page</h1>
+        <?php
+         echo "Hello World";
+         ?>
+         
+    </body>
+</html>
